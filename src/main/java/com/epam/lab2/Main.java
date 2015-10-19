7b18723ed908e0908e4fefeb5013d4ad65e23074
Andy Govorov <thegovorovs@gmail.com>
2015-10-19 14:15:35 +0600

    some link added

--- src/main/java/com/epam/lab2/Main.java
+++ src/main/java/com/epam/lab2/Main.java
@@ -1,9 +1,10 @@
 package com.epam.lab2;
-
+// http://www.javaportal.ru/java/ide/ten_t_eclipse.html
 public class Main 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
     }
 }
